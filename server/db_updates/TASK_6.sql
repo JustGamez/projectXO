77fd5354b02874318044e6fd180372656fc698c0
a.f.larionov <a.f.larionov@gmail.com>
2014-12-16 17:00:51 +0300

    TASK_20, completed, create table games

--- server/db_updates/TASK_6.sql
+++ server/db_updates/TASK_6.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `users`(
-    `id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `id` INT(11) unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `firstName` VARCHAR(128) NOT NULL,
     `lastName` VARCHAR(128) NOT NULL,
-    `socNetUserId` INT(11) NOT NULL,
-    `socNetTypeId` INT(11) NOT NULL,
-    `createTimestamp` INT(11) NOT NULL,
-    `lastLoginTimestamp` INT(11) NOT NULL,
-    `score` INT(11) NOT NULL
+    `socNetUserId` INT(11) unsigned NOT NULL,
+    `socNetTypeId` INT(11) unsigned NOT NULL,
+    `createTimestamp` INT(11) unsigned NOT NULL,
+    `lastLoginTimestamp` INT(11) unsigned NOT NULL,
+    `score` INT(11) unsigned NOT NULL
 )
 ENGINE=InnoDB
 DEFAULT CHARACTER SET utf8
