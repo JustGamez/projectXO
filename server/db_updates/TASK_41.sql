fc6bd53a01a79d70a0a8a46159459ecd4c9c90d1
a.f.larionov <a.f.larionov@gmail.com>
2015-01-16 17:18:44 +0300

    TASK_41, finished, rating table created

---
+++ server/db_updates/TASK_41.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `rating`;
+
+CREATE TABLE `rating` (
+    `userId` INT(11) unsigned NOT NULL,
+    `score` INT(11) NOT NULL DEFAULT 0,
+    `position` INT(11) NOT NULL DEFAULT 0,
+    `updated` INT(11) NOT NULL DEFAULT 0
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
